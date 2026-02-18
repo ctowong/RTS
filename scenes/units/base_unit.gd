54dddb9de75d5876572f3d02e07560f56963fc88
Chris W <christowong@gmail.com>
2026-02-18 05:18:30 -0500

    Migrated movement and attacking to use a state machine

--- scenes/units/base_unit.gd
+++ scenes/units/base_unit.gd
@@ -1,45 +1,19 @@
+class_name BaseUnit
 extends CharacterBody2D
 
 @export var speed: float = 100.0
 
-@onready var sprite: AnimatedSprite2D = %Sprite
-var is_attacking: bool = false
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+@onready var animation_player: AnimatedSprite2D = %AnimationPlayer
+@onready var unit_state_machine: UnitStateMachine = %UnitStateMachine
 
 
 func _input(event: InputEvent) -> void:
-	if event.is_action_pressed("unit_attack"):
-		_trigger_attack_animation()
-	
-# gets the user keyboard input for testing
-func get_keyboard_input() -> void:
-	var input_direction = Input.get_vector("unit_move_left", "unit_move_right", "unit_move_up", "unit_move_down")
-	velocity = input_direction * speed
+	unit_state_machine.on_input(event)
 
 
-func _physics_process(delta: float) -> void:
-	get_keyboard_input()
-	
-	if !is_attacking:
-		if velocity.abs().x > 0 or velocity.abs().y > 0:
-			sprite.play("run")
-		else:
-			sprite.play("idle")
-		
-		if velocity.x < 0:
-			sprite.flip_h = true
-		elif velocity.x > 0:
-			sprite.flip_h = false
-		
-		move_and_slide()
+func _ready() -> void:
+	unit_state_machine.init(self)
 
 
-func _trigger_attack_animation() -> void:
-	is_attacking = true
-	sprite.play("attack1")
-	print("attacking!")
-	sprite.animation_finished.connect(func (): is_attacking = false)
-	pass
+func _physics_process(_delta: float) -> void:
+	unit_state_machine.move_and_slide()
---
+++ scenes/units/unit_states/unit_running_state.gd
@@ -0,0 +1,21 @@
+extends UnitState
+
+
+func enter() -> void:
+	unit.animation_player.play("run")
+
+
+func on_input(event: InputEvent) -> void:
+	if event.is_action("unit_attack"):
+		transition_requested.emit(self, UnitState.State.ATTACKING)
+
+
+func move_and_slide() -> bool:
+	var input_direction = Input.get_vector("unit_move_left", "unit_move_right", "unit_move_up", "unit_move_down")
+	unit.velocity = input_direction * unit.speed
+	if unit.velocity.x < 0:
+		unit.animation_player.flip_h = true
+	elif unit.velocity.x > 0:
+		unit.animation_player.flip_h = false
+	
+	return unit.move_and_slide()
