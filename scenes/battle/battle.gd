d1b638accb37a55d633ef4fa434daf5fe272f393
Chris W <christowong@gmail.com>
2026-02-25 06:00:07 -0500

    Added enemy warrior and archer as separate set of units and animations

--- scenes/battle/battle.gd
+++ scenes/battle/battle.gd
@@ -5,8 +5,6 @@ extends Node2D
 @onready var player_unit_input_handler: PlayerUnitInputHandler = %PlayerUnitInputHandler
 @onready var battle_unit_selection_ui : BattleUnitSelectionUI = %BattleUnitSelectionUI
 
-@onready var base_unit: BaseUnit = $UnitHandler/BaseUnit
-@onready var base_unit_2: BaseUnit = $UnitHandler/BaseUnit2
 
 func _ready() -> void:
 	if not(is_node_ready()):
